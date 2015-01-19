b0ac28a55810e18dd4a12ff800b7c990c87480cb
Mary Jenel <mmyers7@hawaii.edu>
2015-01-19 12:54:31 -0800

    Initial Commit

---
+++ MeetMeup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetMeup
+//
+//  Created by Mary Jenel Myers on 1/19/15.
+//  Copyright (c) 2015 Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
